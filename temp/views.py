77eb33bda577b062ab177d70e69d1a94b899929c
Tomislav Dukez <tomdu3@ymail.com>
2025-05-01 21:53:48 +0100

    add temp app to django project

---
+++ temp/views.py
@@ -0,0 +1,11 @@
+# from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the temp index of a Django app.")
+
+
+def about(request):
+    html_content = "<h1>Hii.. from Django Server | This is a <b>About Page... </b> <br> <p> This about page can be used to see the about section.</p>"
+    return HttpResponse(html_content)
