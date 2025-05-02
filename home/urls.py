6801acfb7b94754608e8243d7b870854333642b2
Tomislav Dukez <tomdu3@ymail.com>
2025-05-02 11:44:20 +0100

    update templates with direct django links

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import index, about, contact
 
 urlpatterns = [
-    path("", index, name="index"),
+    path("", index, name="home"),
     path("about/", about, name="about"),
     path("contact/", contact, name="contact")
 ]
