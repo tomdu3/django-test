8ce4253c68715e4f9251ae42890d700adb2af999
Tomislav Dukez <tomdu3@ymail.com>
2025-05-09 22:54:54 +0100

    update commented test entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,10 @@ python manage.py makemigrations
 python manage.py migrate
 echo 'Migrations complete.'
 
+# echo 'Updating currencies...'
+# ./manage.py update_exchange_rates --all
+# echo 'Currencies updated.'
+
 echo 'Running server...'
 python manage.py runserver 0.0.0.0:8000
 
